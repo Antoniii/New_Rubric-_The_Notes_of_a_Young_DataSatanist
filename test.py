c65d880bbaca498768aefd056d6d0807378b07cd
Addams <anto-kha1@mail.ru>
2020-04-07 10:39:47 +0400

    test install

---
+++ test.py
@@ -0,0 +1,20 @@
+import tensorflow as tf 
+import keras
+import cv2
+import numpy
+import scipy
+import matplotlib
+
+print('tensorflow ', tf.__version__)
+print('keras ', keras.__version__)
+print('opencv ', cv2.__version__)
+print('numpy ', numpy.__version__)
+print('scipy ', scipy.__version__)
+print('matplotlib ', matplotlib.__version__)
+
+# tensorflow  1.5.0
+# keras  2.2.4
+# opencv  4.1.0
+# numpy  1.16.1
+# scipy  1.3.0
+# matplotlib  3.1.1
